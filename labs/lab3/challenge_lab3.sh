8942a4420e6a1281a8fa467a7141c113a806808a
AhmedE99 <aaelbakly@students.pccc.edu>
2024-02-28 04:42:50 +0000

    lab 3 finished

---
+++ labs/lab3/challenge_lab3.sh
@@ -0,0 +1,12 @@
+echo -e "The current date is: "
+date
+
+echo -e "Some information about my system:"
+echo $HOSTNAME
+echo "This is the current user:"
+echo $USER
+
+echo -e "RAM Information"
+echo $RAM
+
+
